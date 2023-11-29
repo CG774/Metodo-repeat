248087101e67ce614a56c7d083be3b4d06149d2e
CG774 <Carlos2004Gordillo@gmail.com>
2023-11-29 14:50:22 -0500

    Actualizacion. Solo se agregaron dos restricciones

--- src/Metodo_Repeat.java
+++ src/Metodo_Repeat.java
@@ -114,13 +114,27 @@ public class Metodo_Repeat extends javax.swing.JFrame {
     private void btnRepetirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRepetirActionPerformed
         // TODO add your handling code here:
         String cadena = txtFrase.getText();
+
+    try {
+        //Veces de repeticion
         int VecesRep = Integer.parseInt(txtVecesrep.getText());
 
-        String cadenaRepeat = cadena.repeat(VecesRep);
-        String mensage = "Cadena original: " + cadena + "\n"
-                + "Cadena repetida " + VecesRep + " veces: " + cadenaRepeat;
+        
+        if (VecesRep > 0) {
+            //Implementacion del metodo repeat
+            String cadenaRepeat = cadena.repeat(VecesRep);
+            String mensaje = "Cadena original: " + cadena + "\n"
+                    + "Cadena repetida " + VecesRep + " veces: " + cadenaRepeat;
 
-        JOptionPane.showMessageDialog(null, mensage);
+            JOptionPane.showMessageDialog(null, mensaje);
+        } else {
+            
+            JOptionPane.showMessageDialog(null, "Por favor, ingrese un número entero positivo para la repetición.");
+        }
+    } catch (NumberFormatException e) {
+        
+        
+    }
 
     }//GEN-LAST:event_btnRepetirActionPerformed
 
@@ -132,35 +146,9 @@ public class Metodo_Repeat extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Metodo_Repeat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Metodo_Repeat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Metodo_Repeat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Metodo_Repeat.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Metodo_Repeat().setVisible(true);
-            }
-        });
+        Metodo_Repeat ventana = new Metodo_Repeat();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
